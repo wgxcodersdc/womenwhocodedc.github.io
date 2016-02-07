849420d46cc16e5c90931e3b68e8fd4964772f62
freeeve <eve.freema@gmail.com>
2016-02-07 16:35:52 -0500

    testing deploy moar

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public "https://$GH_TOKEN@$GH_REF" gh-pages > /dev/null 2>&1
+git subtree push --prefix=public "https://$GH_TOKEN@$GH_REF" gh-pages #> /dev/null 2>&1
