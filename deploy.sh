a1ef1a30360d92c934910e6b3c9e7d92f971f147
freeeve <eve.freema@gmail.com>
2016-02-07 17:07:00 -0500

    why isn't it using the theme

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t wwcdc
+hugo
 
 git config user.name "Travis CI"
 git config user.email "<you>@<your-email>"
