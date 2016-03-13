54739c440786146480c7aed88de9b2e3c5b70162
Eve Freeman <eve.freema@gmail.com>
2016-03-13 17:53:36 -0400

    trying to make travis fail if the build fails.

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,19 @@ git config user.email "<you>@<your-email>"
 # Build the project.
 hugo
 
-# Add changes to git.
-git add -A
+if [ $? -ne 0 ] then
+  # Add changes to git.
+  git add -A
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+  # Commit changes.
+  msg="rebuilding site `date`"
+  if [ $# -eq 1 ]
+    then msg="$1"
+  fi
+  git commit -m "$msg"
+
+  # Push source and build repos.
+  git subtree split --prefix=public -b gh-pages
+  git push -f "https://$GH_TOKEN@$GH_REF" gh-pages:gh-pages > /dev/null 2>&1
+  git branch -D gh-pages
 fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git subtree split --prefix=public -b gh-pages
-git push -f "https://$GH_TOKEN@$GH_REF" gh-pages:gh-pages > /dev/null 2>&1
-git branch -D gh-pages
-
