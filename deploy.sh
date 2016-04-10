70768425416996ab32d9f9add4590a654114e9c4
Alexandra Ulsh <ulsh@mapbox.com>
2016-04-10 17:17:38 -0400

    Only push to gh-pages if on master branch

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mRunning Travis test for WWCDC website\033[0m"
 
-git config user.name "Travis CI"
-git config user.email "<you>@<your-email>"
+#git config user.name "Travis CI"
+#git config user.email "<you>@<your-email>"
 
 # Build the project.
 hugo
 
-toreturn=$?
+# Get the current branch name
+branch_name=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
-if [ $toreturn -eq 0 ] 
-then
-  # Add changes to git.
-  git add -A
+if [ $branch_name = "master" ]
+then 
+  echo -e "\033[0;32m On master branch, publishing to Github Pages\033[0m"
 
-  # Commit changes.
-  msg="rebuilding site `date`"
-  if [ $# -eq 1 ]
-    then msg="$1"
+  toreturn=$?
+
+  if [ $toreturn -eq 0 ] 
+  then
+    # Add changes to git.
+    git add -A
+
+    # Commit changes.
+    msg="rebuilding site `date`"
+    if [ $# -eq 1 ]
+      then msg="$1"
+    fi
+    git commit -m "$msg"
+
+    # Push source and build repos.
+    git subtree split --prefix=public -b gh-pages
+    git push -f "https://$GH_TOKEN@$GH_REF" gh-pages:gh-pages > /dev/null 2>&1
+    git branch -D gh-pages
   fi
-  git commit -m "$msg"
+  exit $toreturn
 
-  # Push source and build repos.
-  git subtree split --prefix=public -b gh-pages
-  git push -f "https://$GH_TOKEN@$GH_REF" gh-pages:gh-pages > /dev/null 2>&1
-  git branch -D gh-pages
 fi
-exit $toreturn
+
+if [ $branch_name != "master" ]
+then 
+
+  echo "Not on master branch, not updating WWCDC public website"
+  echo "On $branch_name branch, not pushing to gh-pages"
+
+fi
