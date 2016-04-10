38ae73f8be845209bdd80b402eeda929ce0634f3
Alexandra Ulsh <ulsh@mapbox.com>
2016-04-10 18:44:03 -0400

    Script exits on Hugo build error

--- deploy.sh
+++ deploy.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
+set -e # exit with nonzero exit code if anything fails
 
-echo -e "\033[0;32mRunning Travis test for WWCDC website\033[0m"
+# Initial hugo build to test the project
+# Output is not user-friendly, so this first build just catches any errors
+HUGO_BUILD=$(hugo)
 
-# Set git config so Travis doesn't complain that it doesn't know who we are
+if [[ $HUGO_BUILD == *ERROR* ]]
+  then
+  echo "There are errors in your Hugo build, aborting script"
+  hugo
+  exit 1
+fi
 
-git config user.name "WWCDC Travis CI build"
-git config user.email "wwcodedc@gmail.com"
+echo "Building Hugo website"
 
-# Build the project.
+# Build the site and use standard output
 hugo
+
+echo "Hugo build succeeded!"
 
 # Only push to gh-pages if on the master branch
 
 if [[ $TRAVIS_BRANCH = "master" ]]
 then 
+
   echo -e "\033[0;32m On $TRAVIS_BRANCH branch, publishing to Github Pages\033[0m"
 
-  toreturn=$?
+  # Set git config so Travis doesn't complain that it doesn't know who we are
 
-  if [ $toreturn -eq 0 ] 
-  then
-    # Add changes to git.
-    git add -A
+  git config user.name "WWCDC Travis CI build"
+  git config user.email "wwcodedc@gmail.com"
 
-    # Commit changes.
-    msg="rebuilding site `date`"
-    if [ $# -eq 1 ]
-      then msg="$1"
-    fi
-    git commit -m "$msg"
+  # Add changes to git.
+  git add -A
 
-    # Push source and build repos.
-    git subtree split --prefix=public -b gh-pages
-    git push -f "https://$GH_TOKEN@$GH_REF" gh-pages:gh-pages > /dev/null 2>&1
-    git branch -D gh-pages
+  # Commit changes.
+  msg="rebuilding site `date`"
+  if [ $# -eq 1 ]
+    then msg="$1"
   fi
-  exit $toreturn
+  git commit -m "$msg"
+
+  # Push source and build repos.
+  git subtree split --prefix=public -b gh-pages
+  git push -f "https://$GH_TOKEN@$GH_REF" gh-pages:gh-pages > /dev/null 2>&1
+  git branch -D gh-pages
 
 fi
 
